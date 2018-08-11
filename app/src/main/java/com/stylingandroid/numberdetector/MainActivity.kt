8decdc82a312993845a2cac9a92dd21edef8854f
Mark Allison <mark@stylingandroid.com>
2018-08-11 11:50:43 +0100

    Code for Part 2

--- app/src/main/java/com/stylingandroid/numberdetector/MainActivity.kt
+++ app/src/main/java/com/stylingandroid/numberdetector/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var numberClassifier: NumberClassifier
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        numberClassifier = NumberClassifier()
     }
 }
